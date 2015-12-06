58a400dfdb298bf62eec43fb5d853b927ccb2b81
cwkralap <cwkralap@mtu.edu>
2015-12-06 16:22:30 -0500

    Fixed formatting

--- Dungeon.f95
+++ Dungeon.f95
@@ -8,25 +8,33 @@ program dungeon
 				integer :: moxie
 				integer :: hp
 				integer :: mana
+				integer :: xp
+				integer :: score
 	   end type Player
 
 	   !declaring type variables
-	   type(Player) :: player
+	   type(Player) :: p
 
 	   !accessing the components of the structure
-	   player%name= "Son Goku"
-		 player%strength=1
-		 player%intelegence=1
-		 player%moxie=1
-		 player%hp=1
-		 player%mana=1
+	   p%name= "Son Goku"
+		 p%strength=1
+		 p%intelegence=1
+		 p%moxie=1
+		 p%hp=1
+		 p%mana=1
+		 p%xp=0
+		 p%score=0
+
+		 p%strength=p%strength+1
 
 		 !display info
-		 Print *, player%name
-		 Print *, player%strength
-		 Print *, player%intelegence
-		 Print *, player%moxie
-		 Print *, player%hp
-		 Print *, player%mana
+		 Print  "(a10)",adjustl(p%name)
+		 Print  "(a14,i2)",'STRENGTH    = ',p%strength
+		 Print  "(a14,i2)",'INTELEGENCE = ',p%intelegence
+		 Print  "(a14,i2)",'MOXIE       = ',p%moxie
+		 Print  "(a14,i2)",'HP          = ',p%hp
+		 Print  "(a14,i2)",'MANA        = ',p%mana
+		 Print  "(a14,i2)",'XP          = ',p%xp
+		 Print  "(a14,i2)",'SCORE       = ',p%score
 
 end program dungeon
