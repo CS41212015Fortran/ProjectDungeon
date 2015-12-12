e39c17b693b66e742ead49001d63e43f5601fa18
rjfenton <rjfenton@mtu.edu>
2015-12-12 15:06:59 -0500

    implement mob RNG

--- mobs.f95
+++ mobs.f95
@@ -12,11 +12,60 @@ module class_Mob
 
 	contains
 
-	subroutine Mob_print(this)
+  subroutine new_mob(this, d)
+    implicit none
+    type(mob)           :: this
+    type(dungeon_floor) :: d
+    integer :: i, n, clock
+    integer, dimension(:), allocatable :: seed
+    real    :: rrand
+    integer :: irand
+    real    :: dungeon_scale
+    dungeon_scale = (d%get_floor_number / 20)
+    
+    
+    !set up our random stuff
+    call RANDOM_SEED(size = n)
+    allocate(seed(n))
+    
+    call system_clock(count = clock)
+    seed = clock + 37 * (/ (i - 1, i = 1, n) /)
+    call RANDOM_SEED(PUT = seed)
+    deallocate(seed)
+    
+    call RANDOM_NUMBER(rrand)
+    irand = floor(rrand * 5)+1
+    
+    !randomly generate a name
+    if (irand == 5) then
+      this%name = "Skeleton"
+    else if (irand == 4) then
+      this%name = "Wraith"
+    else if (irand == 3) then
+      this%name = "Shade"
+    else if (irand == 2) then
+      this%name = "Zombie"
+    else 
+      this%name = "Slime"
+    end if
+    
+    call RANDOM_NUMBER(rrand)
+    !health
+    irand ceiling(rran * (100 * dungeon_scale)) + (5 * dungeon_scale)
+    this%health = irand
+    
+    call RANDOM_NUMBER(rrand)
+    !strength
+    irand ceiling(rran * (100 * dungeon_scale)) + (5 * dungeon_scale)
+    this%strength = irand
+    
+  end subroutine new_mob
+  
+	subroutine get_info(this)
 		type(Mob), intent(in) :: this
-		print *, 'Mob name is ', this%name
-		print *, this%name, ' has ', this%health, ' points of health.'
-	end subroutine Mob_print
+		!print *, 'Mob name is ', this%name
+		print *, this%name, ' has ', this%health, ' HP.'
+	end subroutine get_info
 
 
 	subroutine attack(att, def)
@@ -28,19 +77,19 @@ module class_Mob
 
 end module class_Mob
 
-program mob_test
-	use class_Mob
-	implicit none
+!program mob_test
+!	use class_Mob
+!	implicit none
 
-	type(Mob) :: e 		! create a mob
-	type(Mob) :: p
+!	type(Mob) :: e 		! create a mob
+!	type(Mob) :: p
 
-	e = Mob('Ureel', 20, 10)
-	call Mob_print(e)
+!	e = Mob('Ureel', 20, 10)
+!	call Mob_print(e)
 
-	p = Mob('Kevin', 20, 10)
-	call attack(p, e)
+!	p = Mob('Kevin', 20, 10)
+!	call attack(p, e)
 
-	call Mob_print(e)
+!	call Mob_print(e)
 
-end program mob_test
+!end program mob_test
