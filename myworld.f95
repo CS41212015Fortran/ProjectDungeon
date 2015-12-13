c66cee6d5d245106818dc92d01d4574e3b117b7a
Kevin Erkkila <krerkkil@colossus.it.mtu.edu>
2015-12-12 22:36:41 -0500

    Fixed stuff

--- myworld.f95
+++ myworld.f95
@@ -1,7 +1,6 @@
 program world
 	use classTrap
 	use classTreasure
-	use class_spells
 	use class_dungeon_floor
 	use classPlayer
 	use class_spells
@@ -13,14 +12,14 @@ program world
 	type(Trap) 					:: t = Trap("bear-trap", 1)
 	type(Treasure)			:: c
 	type(player) 				:: p
-	type(spell)					:: fireball
+	type(spell) 				:: fireball
 	type(spell)					:: heal
 	type(dungeon_floor) :: d
 	character(len=32) 	:: command
 	real 								:: rrand
 
 	!init player
-	print '(a19)',' Enter player name '
+	print *,' Enter player name '
 	read (*,'(A)') p%name
 	p%strength    =1
 	p%intelegence =1
@@ -33,26 +32,24 @@ program world
 	call update_derived_stats(p)
 
 	!init spellbook
-	fireball%name     ="Fireball"
-	fireball%mana_cost=30
-	fireball%dice_roll=20
-	fireball%known    =.false.
 
+	fireball%name      = "Fireball"
+	fireball%mana_cost = 30
+	fireball%dice_roll = 40
 
-	heal%name     ="Heal"
-	heal%mana_cost=30
-	heal%dice_roll=20
-	heal%known    =.false.
+	heal%name      = "Heal"
+	heal%mana_cost = 30
+	heal%dice_roll = 40
 
 	print *,''
-  print '(a12a32)','Good Morrow ', p%name
-	print '(a24)','Time to boost your stats'
-	print '(a53)','You major stats are Strength, Intelegence, and Moxie'
-	print '(a78)','Strength determines you max HP and how much damage your melee attacks will do'
-	print '(a90)','Intelegence determines your max Mana, your Perception, and how much damage spells will do'
-	print '(a91)','Moxie determines your chance for Dodging, Disarming traps, and other "Rouge-like" abilites'
-	print '(a57)','Entering the following chacter will boost that stat by 1'
-	print '(a14)','Valid Commands are "s" for strength, "i" for intelegence, and "m" for moxie'
+	print *,adjustl('Good Morrow '), p%name
+	print *,adjustl('Time to boost your stats')
+	print *,adjustl('You major stats are Strength, Intelegence, and Moxie')
+	print *,adjustl('Strength determines you max HP and how much damage your melee attacks will do')
+	print *,adjustl('Intelegence determines your max Mana, your Perception, and how much damage spells will do')
+	print *,adjustl('Moxie determines your chance for Dodging, Disarming traps, and other "Rouge-like" abilites')
+	print *,adjustl('Entering the following chacter will boost that stat by 1')
+	print *,adjustl('Valid Commands are "s" for strength, "i" for intelegence, and "m" for moxie')
 	print *,''
 	call player_level_up(p)
 	print *,''
@@ -62,7 +59,6 @@ program world
 	read (*,'(A)') command
 
 	main: do while(.true.)
-
 		if(index(command, "combat") > 0) then
 			! TODO Should Mobs have moxie?  To see who gets the initiative
 			!start combat
@@ -80,6 +76,7 @@ program world
 				read (*,'(A)') command
 
 				if(index(command, "attack") > 0) then
+
 					call RANDOM_NUMBER(rrand)
 					if(rrand > (1 - d%mob%dodge_chance)) then
 						print *, "The ", trim(d%mob%name), " dodged your attack!"
@@ -90,6 +87,7 @@ program world
 					end if
 
 				else if(index(command, "magic") > 0) then
+
 					print *, "Enter F to shoot a Fireball or H to heal yourself"
 					read (*,'(A)') command
 					if (command.eq.'F' .or. command.eq.'f') then
@@ -97,10 +95,10 @@ program world
 					else
 						call apply_defensive_spell(heal,p)
 					end if
-				else if(index(command, "run") > 0) then
-					print *, "You ran away from the ", d%mob%name
-					exit combat
 
+				else if(index(command, "run") > 0) then
+					print *, "You ran away from the ", trim(d%mob%name)
+					exit combat
 				else
 					print *, "I don't know what you mean by ",command
 				end if
@@ -202,7 +200,7 @@ program world
 	  	!If the room contains a treasure chest
 			if (d%has_treasure) then
 				print*, "There is a locked treasure chest in this room."
-				call unlockChest(c, p, d)
+				call unlockChest(c, p)
 			end if
 
 		! Buy an item from a shop
@@ -222,18 +220,4 @@ program world
 		else if(index(command, "check-item") > 0) then
 
 	  ! Look around you to gather your bearings
-	  else if(index(command, "look") > 0) then
-			call examine_room(d)
-
-		! Quit the game
-		else if(index(command, "quit") > 0) then
-			exit main
-		else
-			print *, "I don't know what you mean by ",command
-		end if
-
-		read (*,'(A)') command
-	end do main
-  !calculate the score
-	print *, p%name,"'s Quest has come to an end: Your Final Score is ",get_score(p)
-end program world
+	  else if(index(command, "look") > 0) t
